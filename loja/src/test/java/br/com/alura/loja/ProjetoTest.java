2ffad5ab3fbf173ff19b94f3bc073f60986b9524
phlira <pedrohsoares.lira@gmail.com>
2016-02-02 17:09:34 -0200

    Cap 1 Completo.

---
+++ loja/src/test/java/br/com/alura/loja/ProjetoTest.java
@@ -0,0 +1,21 @@
+package br.com.alura.loja;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class ProjetoTest {
+
+	@Test
+	public void buscaPrimeiroProjetoDoDAO(){
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo = target.path("/projetos").request().get(String.class);
+		Assert.assertTrue(conteudo.contains("<nome>Minha loja</nome>"));
+	}
+	
+}
